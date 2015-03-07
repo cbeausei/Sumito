76f700a5a7f30e13bdb5fb84ff0b45c393b83a8c
astrubel <axel.strubel@telecom-bretagne.eu>
2015-03-07 14:07:27 +0100

    Juste un petit test

--- app/src/main/java/com/example/clement/sumito/FirstPage.java
+++ app/src/main/java/com/example/clement/sumito/FirstPage.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+ // COMMENTAIRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRE TESTTTTTTT
 
 public class FirstPage extends ActionBarActivity {
 
@@ -36,4 +36,5 @@ public class FirstPage extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }

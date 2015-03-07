098fd60b43a6fc0ab52cd446e538c262696c607a
Clement <clementbeauseigneur@hotmail.fr>
2015-03-07 11:09:30 +0100

    test

--- app/src/main/java/com/example/clement/sumito/FirstPage.java
+++ app/src/main/java/com/example/clement/sumito/FirstPage.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class FirstPage extends ActionBarActivity {
-
+//comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

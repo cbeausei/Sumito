81969e6eafb9bdc3efb78f0b18d74f66bf5c53bc
Bruno <bruno.quercia@telecom-bretagne.eu>
2015-04-04 22:20:13 +0200

    Ajout du plateau

--- app/src/main/java/com/example/clement/sumito/DrawView.java
+++ app/src/main/java/com/example/clement/sumito/DrawView.java
@@ -1,40 +1,53 @@
 package com.example.clement.sumito;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
+
+import com.example.clement.sumito.R;
 
 public class DrawView extends View {
     Paint paint = new Paint();
     Canvas canvas = new Canvas();
-    Bitmap boule;
+    Bitmap boule, plateau;
     float x = 10, y = 20;
+    int w, h, width, height;
+
     public DrawView(Context context) {
         super(context);
         boule = BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire);
+        plateau = BitmapFactory.decodeResource(getResources(), R.drawable.cadre);
     }
 
     @Override
     public void onDraw(final Canvas canvas) {
         this.canvas = canvas;
-        final int h = canvas.getHeight();
-        final int w = canvas.getWidth();
-        boule = Bitmap.createScaledBitmap(boule, h/20, h/20, true);
-        canvas.drawBitmap(boule, x-h/40, y-h/40, null);
+        h = canvas.getHeight();
+        w = canvas.getWidth();
+        if (1073 * w < 1420 * h) {
+            width = w;
+            height = 1073 * w / 1420;
+        } else {
+            height = h;
+            width = 1420 * h / 1073;
+        }
+
+        boule = Bitmap.createScaledBitmap(boule, height / 10, height / 10, true);
+        plateau = Bitmap.createScaledBitmap(plateau, width, height, true);
+        canvas.drawBitmap(plateau, (w - width) / 2, (h - height) / 2, null);
+        canvas.drawBitmap(boule, x - h / 40, y - h / 40, null);
     }
 
-    public void dessinerBoule(final Canvas canvas){
+    public void dessinerBoule(final Canvas canvas) {
         canvas.drawBitmap(boule, x, y, null);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event){
+    public boolean onTouchEvent(MotionEvent event) {
         x = event.getRawX();
         y = event.getRawY();
         this.invalidate();

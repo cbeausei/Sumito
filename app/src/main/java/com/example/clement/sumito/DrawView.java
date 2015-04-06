96da9df744b5a2b4773034b42c98d8b138ba7004
Bruno <bruno.quercia@telecom-bretagne.eu>
2015-04-06 16:53:43 +0200

    Ajout d'un arrière-plan Plateau vu de dessus 28 boules bien placées À revoir en termes de structure

--- app/src/main/java/com/example/clement/sumito/DrawView.java
+++ app/src/main/java/com/example/clement/sumito/DrawView.java
@@ -7,20 +7,60 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
+import java.lang.Math;
 
 import com.example.clement.sumito.R;
 
 public class DrawView extends View {
     Paint paint = new Paint();
     Canvas canvas = new Canvas();
-    Bitmap boules[], plateau;
+    Bitmap boulesNoires[], boulesBlanches[], plateau, fond;
+    int[] posYn = new int[]{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2};
+    int[] posXn = new int[]{4, 5, 6, 7, 8, 3, 4, 5, 6, 7, 8, 4, 5, 6};
+    int[] posYb = new int[]{8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 6, 6, 6};
+    int[] posXb = new int[]{0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 5, 2, 3, 4};
     float x = 10, y = 20;
     int w, h, width, height;
 
     public DrawView(Context context) {
         super(context);
-        boules = new Bitmap[] {BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire), BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire)};
+        boulesNoires = new Bitmap[] {BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.boulenoire)};
+        boulesBlanches = new Bitmap[] {BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche),
+                               BitmapFactory.decodeResource(getResources(), R.drawable.bouleblanche)};
         plateau = BitmapFactory.decodeResource(getResources(), R.drawable.cadre);
+        fond = BitmapFactory.decodeResource(getResources(), R.drawable.fond);
+    }
+
+    private int[] convertCoordinates(int x, int y){
+        int absc = (int)(((x+y/2.0-2.0)*45/516.0+29/129.0-1/24.0)*height + (w-width)/2.0);
+        y = (int)((y*78/8.0/129.0+26/129.0-1/24.0)*height + (h-height)/2.0);
+        x = absc;
+        return(new int[]{x, y});
     }
 
     @Override
@@ -28,20 +68,28 @@ public class DrawView extends View {
         this.canvas = canvas;
         h = canvas.getHeight();
         w = canvas.getWidth();
-        if (1073 * w < 1420 * h) {
+
+        if (1290 * w < 1487 * h) {
             width = w;
-            height = 1073 * w / 1420;
+            height = 1290 * w / 1487;
         } else {
             height = h;
-            width = 1420 * h / 1073;
+            width = 1487 * h / 1290;
         }
-
-        boules[0] = Bitmap.createScaledBitmap(boules[0], height / 10, height / 10, true);
-        boules[1] = Bitmap.createScaledBitmap(boules[0], height / 10, height / 10, true);
         plateau = Bitmap.createScaledBitmap(plateau, width, height, true);
+        fond = Bitmap.createScaledBitmap(fond, Math.max(w, h), Math.max(w, h), true);
+        canvas.drawBitmap(fond, 0, 0, null);
         canvas.drawBitmap(plateau, (w - width) / 2, (h - height) / 2, null);
-        canvas.drawBitmap(boules[0], x - h / 40, y - h / 40, null);
-        canvas.drawBitmap(boules[1], x + h / 40, y + h / 40, null);
+        for(int i = 0 ; i < 14 ; i++) {
+            boulesNoires[i] = Bitmap.createScaledBitmap(boulesNoires[i], height/12, height/12, true);
+            boulesBlanches[i] = Bitmap.createScaledBitmap(boulesBlanches[i], height/12, height/12, true);
+            int coordinatesn[] = new int[]{};
+            int coordinatesb[] = new int[]{};
+            coordinatesn = convertCoordinates(posXn[i], posYn[i]);
+            coordinatesb = convertCoordinates(posXb[i], posYb[i]);
+            canvas.drawBitmap(boulesNoires[i], coordinatesn[0], coordinatesn[1], null);
+            canvas.drawBitmap(boulesBlanches[i], coordinatesb[0], coordinatesb[1], null);
+        }
     }
 
     @Override

e2702983ef5d1171bd261f0e617b03dd7e113d0e
cbeausei <abalones2>
2015-04-08 12:25:32 +0200

    Ajout de la classe Bot qui permet d'instancier un IA (voir dans la classe Test comment on peut s'en servir) Ajout de sous-classes servant à l'IA pour calculer la liste des mouvement possibles : Move, MoveList, MoveWay, MoveWayList Ajout enfin de la classe Board qui permet de représenter le plateau, et qui permet en particulier à l'IA d'effectuer ses tests.

---
+++ app/src/main/java/com/example/clement/sumito/MoveWayList.java
@@ -0,0 +1,49 @@
+package com.example.clement.sumito;
+
+/**
+ * Created by Clement on 08/04/2015.
+ */
+public class MoveWayList {
+    private MoveWay moveWay;
+    private MoveWayList next;
+
+    public MoveWayList() {
+        moveWay=new MoveWay();
+        next=null;
+    }
+
+    public MoveWayList(MoveWay moveWay) {
+        this.moveWay=moveWay;
+        this.next=null;
+    }
+
+    public void addMoveWay(MoveWay moveWay) {
+        if (next==null) {
+            next=new MoveWayList(moveWay);
+            return;
+        }
+        next.addMoveWay(moveWay);
+        return;
+    }
+
+    public MoveWay getMoveWay() {
+        return moveWay;
+    }
+
+    public MoveWayList getNext() {
+        return next;
+    }
+
+    public Boolean isEqual(MoveWayList moveWayList) {
+        if (moveWayList==null) return true;
+        if (next==null) return ((moveWay.isEqual(moveWayList.getMoveWay()))&(moveWayList.getNext()==null));
+        return((moveWay.isEqual(moveWayList.getMoveWay()))&next.isEqual(moveWayList.getNext()));
+    }
+
+    public void display() {
+        moveWay.display();
+        if (next!=null) next.display();
+        if (next==null) System.out.println();
+    }
+
+}

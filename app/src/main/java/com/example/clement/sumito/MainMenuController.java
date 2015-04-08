a34b5f1fb4f85708ab841c2546a80f52b4048bd9
astrubel <axel.strubel@telecom-bretagne.eu>
2015-04-08 11:33:28 +0200

    Modifications des menus pour toutes les tailles d'écrans ( à partir de 3,2")

--- app/src/main/java/com/example/clement/sumito/MainMenuController.java
+++ app/src/main/java/com/example/clement/sumito/MainMenuController.java
@@ -3,8 +3,6 @@ package com.example.clement.sumito;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -51,7 +49,7 @@ public class MainMenuController extends Activity {
 
     }
 
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -74,4 +72,5 @@ public class MainMenuController extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+*/
 }
